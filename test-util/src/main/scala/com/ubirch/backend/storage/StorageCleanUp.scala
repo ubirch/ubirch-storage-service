a8a260a8c7475d9aab4575ab581b2f7ea3917f23
cvandrei <crille@indarium.de>
2016-08-22 16:01:28 +0200

    bugfix: elastic search indexes were not deleted

--- test-util/src/main/scala/com/ubirch/backend/storage/StorageCleanUp.scala
+++ test-util/src/main/scala/com/ubirch/backend/storage/StorageCleanUp.scala
@@ -2,6 +2,7 @@ package com.ubirch.backend.storage
 
 import java.net.URL
 
+import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 import com.ubirch.backend.storage.config.{ServerConfig, ServerConst}
 import uk.co.bigbeeconsultants.http.HttpClient
@@ -12,6 +13,8 @@ import uk.co.bigbeeconsultants.http.HttpClient
   */
 trait StorageCleanUp extends LazyLogging {
 
+  private lazy val conf = ConfigFactory.load
+
   /**
     * Delete everything in the indexes specified in [ServerConst.ES_ALL_INDEXES].
     */
@@ -20,7 +23,7 @@ trait StorageCleanUp extends LazyLogging {
     logger.info("start storage clean up")
     val httpClient = new HttpClient
     ServerConst.ES_ALL_INDEXES foreach { idx =>
-      httpClient.delete(new URL(s"${ServerConfig.esUrl}/$idx"))
+      httpClient.delete(new URL(s"${ServerConfig.esUrl}/${conf.getString(idx)}"))
     }
 
   }
