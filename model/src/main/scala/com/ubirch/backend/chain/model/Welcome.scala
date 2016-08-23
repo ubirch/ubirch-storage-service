04d734848887ba594083e6c2b91528859c4e0a6b
cvandrei <crille@indarium.de>
2016-08-23 17:46:40 +0200

    add Welcome class to model

---
+++ model/src/main/scala/com/ubirch/backend/chain/model/Welcome.scala
@@ -0,0 +1,9 @@
+package com.ubirch.backend.chain.model
+
+/**
+  * author: cvandrei
+  * since: 2016-07-27
+  */
+case class Welcome(version: String = "1.0",
+                   status: String = "OK",
+                   message: String)
