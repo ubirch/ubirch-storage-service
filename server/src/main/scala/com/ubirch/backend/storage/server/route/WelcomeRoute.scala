c2c1ffb1223d04a02484bbb94da27112b3dffe4b
cvandrei <crille@indarium.de>
2016-08-24 15:30:54 +0200

    fix broken import

--- server/src/main/scala/com/ubirch/backend/storage/server/route/WelcomeRoute.scala
+++ server/src/main/scala/com/ubirch/backend/storage/server/route/WelcomeRoute.scala
@@ -3,7 +3,7 @@ package com.ubirch.backend.storage.server.route
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import com.ubirch.backend.storage.server.model.Welcome
-import com.ubirch.backend.util.MyJsonProtocol
+import com.ubirch.util.json.MyJsonProtocol
 import de.heikoseeberger.akkahttpjson4s.Json4sSupport._
 
 /**
